53511708e80fe9d4ccf5edc4021324967174e8f5
Chris Willette <christopher.willette@gmail.com>
2016-04-07 02:16:57 -0700

    working

--- makefile
+++ makefile
@@ -1,18 +1,18 @@
 #chris willette
-#cs360 hw6 (philo)
+#cs360 hw7 (philo)
 
 CC = gcc
 CFLAGS = -g -std=gnu99 -Wall -Wextra -pedantic
 RANDOM = /encs_share/cs/class/cs360
 
 philo: philo.o random_r.o
-	$(CC) $(CFLAGS) random_r.o philo.o -o philo -lpthread -lm
+	$(CC) $(CFLAGS) random_r.o philo.o -o philo -lm -lrt
 
 philo.o: philo.c
 	$(CC) $(CFLAGS) -c -I$(RANDOM)/include/ philo.c
 
-random_r.o: #random.c random.h
-	$(CC) $(CLAGS) -c -I$(RANDOM)/include/ $(RANDOM)/src/random_r.c -lpthread -lm
+random_r.o: 
+	$(CC) $(CLAGS) -c -I$(RANDOM)/include/ $(RANDOM)/src/random_r.c -lm -lrt
 
 clean:
 	-rm -rf *.o philo
--- philo.c
+++ philo.c
@@ -9,88 +9,66 @@
 #define NUM_PHILOS 5
 #define MAX_EAT_TIME 100
 
-
-typedef struct _philo{
-    pthread_t thread;
-    int id;
-} philo;
-
 pthread_mutex_t chopSticks[NUM_PHILOS]; //locks on the chopsticks
-philo philos[NUM_PHILOS]; //philosophers
+pthread_t philos[NUM_PHILOS];
+int philos_id[NUM_PHILOS];
 
+void* dinner(void* arg){
+  int philoNum = *(int *)arg;
+  unsigned int philo_u  = *(unsigned int *)arg;
+  int totalEatTime = 0;
+  int eatClock;
+  int thinkClock;
+  int rightStick;
+  int leftStick;
+  leftStick = philoNum;
+  rightStick = philoNum - 1;
 
-int eatTime(unsigned int person){
-    int interval = randomGaussian_r(9, 3, &person);
-    if(interval < 0) interval = 0;
-    return interval;
-}
+  if(philoNum == 0){
+    leftStick = NUM_PHILOS - 1;
+    rightStick = philoNum;
+  }
 
-
-int thinkTime(unsigned int person){
-    int interval = randomGaussian_r(11, 7, &person);
-    if(interval < 0) interval = 0;
-    return interval;
-}
-
-
-void* philosophize(void* arg){
-    int philoNum = *(int *)arg;
-    unsigned int philo_u  = *(unsigned int *)arg;
-    int totalEatTime = 0;
-    int eatClock;
-    int thinkClock;
-    int rightStick;
-    int leftStick;
-    leftStick = philoNum;
-    rightStick = philoNum - 1;
-
-    if(philoNum == 0){
-	leftStick = NUM_PHILOS - 1;
-	rightStick = philoNum;
-    }
-
-    while(totalEatTime < MAX_EAT_TIME){
-
-	thinkClock = thinkTime(philo_u);
-	sleep(thinkClock);
-	printf("\nPhilosopher %d now thinking for %d seconds.",
-	       philoNum+1, thinkClock);
-
-
-	//grab chopsticks
-        pthread_mutex_lock(&chopSticks[leftStick]);
-        pthread_mutex_lock(&chopSticks[rightStick]);
-	//eat
-	eatClock = eatTime(philo_u);
-	sleep(eatClock);
-	printf("\nPhilosopher %d now eating for %d seconds.", 
-	       philoNum+1, eatClock);
-	totalEatTime = totalEatTime + eatClock;
-
-        pthread_mutex_unlock(&chopSticks[leftStick]);
-        pthread_mutex_unlock(&chopSticks[rightStick]);
-    }
-    printf("\nPhilosopher %d has left the building.\n", philoNum+1);
-    return NULL;
+  while(totalEatTime < MAX_EAT_TIME){
+    
+    thinkClock = randomGaussian_r(9, 3, &philo_u);
+    if(thinkClock < 0) thinkClock = 0;
+    printf("\nPhilosopher %d now thinking for %d seconds.",
+	   philoNum+1, thinkClock);
+    sleep(thinkClock); 
+    //grab chopsticks
+    pthread_mutex_lock(&chopSticks[leftStick]);
+    pthread_mutex_lock(&chopSticks[rightStick]);
+    //eat
+    eatClock = randomGaussian_r(11, 7, &philo_u);
+    if(eatClock < 0) eatClock = 0;
+    printf("\nPhilosopher %d now eating for %d seconds.", 
+	   philoNum+1, eatClock);
+    sleep(eatClock);     
+    totalEatTime = totalEatTime + eatClock;
+    pthread_mutex_unlock(&chopSticks[leftStick]);
+    pthread_mutex_unlock(&chopSticks[rightStick]);
+  }
+  printf("\nPhilosopher %d has left the building.\n", philoNum+1);
+  return NULL;
 }
 
 
 int main(){
-    int i;
-    for(i=0; i<NUM_PHILOS; i++){
-        pthread_mutex_init(&chopSticks[i],NULL);
-        philos[i].id = i;
-    }
-    for(i=0;i<NUM_PHILOS;i++){
-        pthread_create(&philos[i].thread,NULL, philosophize,(void *)&philos[i].id);
-    }
-    for(i=0;i<NUM_PHILOS;i++){
-        pthread_join(philos[i].thread,NULL);
-    }
-    for(i=0;i<NUM_PHILOS;i++){
-        pthread_mutex_destroy(&chopSticks[i]);
-    }
-
-    printf("\nEveryone's done and left the table.\n\n");
-    return 0;
+  int i;
+  for(i=0; i<NUM_PHILOS; i++){
+    pthread_mutex_init(&chopSticks[i],NULL);
+    philos_id[i] = i;
+  }
+  for(i=0;i<NUM_PHILOS;i++){
+    pthread_create(&philos[i],NULL, dinner,(void *)&philos_id[i]);
+  }
+  for(i=0;i<NUM_PHILOS;i++){
+    pthread_join(philos[i],NULL);
+  }
+  for(i=0;i<NUM_PHILOS;i++){
+    pthread_mutex_destroy(&chopSticks[i]);
+  }
+  printf("\nEveryone's done and left the table.\n\n");
+  return 0;
 }
